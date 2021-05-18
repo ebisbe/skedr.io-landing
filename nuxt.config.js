1b58c91c10ab9f5802f662df539e00a88b369316
Enric Bisbe Gil <enricu@gmail.com>
2021-05-18 18:47:04 +0200

    Remove LogRocket from landing page

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,15 +21,6 @@ module.exports = {
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
     script: [
       {
-        src: 'https://cdn.logrocket.io/LogRocket.min.js',
-        crossorigin: 'anonymous'
-      },
-      {
-        innerHTML: "window.LogRocket && window.LogRocket.init('zye3nm/skedrio-prod', { rootHostname: 'skedr.io' });",
-        type: 'text/javascript',
-        charset: 'utf-8'
-      },
-      {
         innerHTML:
           '!function(e,t,n){function a(){var e=t.getElementsByTagName("script")[0],n=t.createElement("script");n.type="text/javascript",n.async=!0,n.src="https://beacon-v2.helpscout.net",e.parentNode.insertBefore(n,e)}if(e.Beacon=n=function(t,n,a){e.Beacon.readyQueue.push({method:t,options:n,data:a})},n.readyQueue=[],"complete"===t.readyState)return a();e.attachEvent?e.attachEvent("onload",a):e.addEventListener("load",a,!1)}(window,document,window.Beacon||function(){});',
         type: 'text/javascript'
